ebe685e99e9afb858f8cc4ceec4755903f73118b
Duy Nguyen <duy@MacBook-Pro-cua-Duy.local>
2021-02-27 21:46:37 +0700

    try error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 
-const app = express();
+const apps = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
