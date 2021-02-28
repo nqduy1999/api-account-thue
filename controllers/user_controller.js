dfd31134752b000b7ab5db29ef62b8772020de29
Duy Nguyen <duy@MacBook-Pro-cua-Duy.local>
2021-03-01 01:06:39 +0700

    get Access Token

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -57,12 +57,38 @@ const UserController = {
       const {email, password} = req.body;
       const user = await User.findOne({email});
       if(!user) return res.status(400).json({msg : "Email không tồn tại vui lòng kiểm tra lại"});
-      const isMatchPass = bcrypt.compare(password, user.password);
-      if(!isMatchPass) return res.status(400).json({msg : "Password is inCorrect"});
+      const isMatchPass = await bcrypt.compare(password, user.password);
+      console.log(isMatchPass);
+      if(!isMatchPass) return res.status(400).json({msg : "Mat khau khong dung vui long nhap lai"});
+      const refresh_token = createRefreshToken({id : user._id});
+      res.cookie('refreshToken', refresh_token, {
+        httpOnly : true,
+        path : '/user/refresh_token',
+        maxAge: 7*24*60*60*1000
+      });
+      res.json({
+        msg : "Đăng nhập thành công"
+      })
     } catch (err) {
       return res.status(500).json({msg :err.message})
     }
-  }
+  },
+  getAccessToken: (req, res) => {
+    try {
+        const rf_token = req.cookies.refreshToken
+        console.log(rf_token);
+        if(!rf_token) return res.status(400).json({msg: "Please login now!"})
+        jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) =>{
+          if(err) return res.status(400).json({msg: "Please login now!"})
+          const access_token = createAccessToken({id: user.id});
+          res.json({
+            access_token
+          })
+        });
+    } catch (err) {
+        return res.status(500).json({msg: err.message})
+    }
+},
 };
 
 function validateEmail(email) {
--- routes/user_router.js
+++ routes/user_router.js
@@ -3,5 +3,6 @@ const userController = require('../controllers/user_controller');
 
 router.post('/register', userController.register);
 router.post('/activation', userController.activateEmail);
-router.post('/login', userController.login)
+router.post('/login', userController.login);
+router.post('/refresh_token', userController.getAccessToken);
 module.exports = router;
